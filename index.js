7c35ffd4d2ae69d4f239e97ea87bf11ba7d18b7d
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2022-12-20 23:12:34 -0600

    cleanup, about to attempt first build

--- index.js
+++ index.js
@@ -11,15 +11,6 @@ app.get("/data", function (req, res) {
   res.json({ foo: "bar" });
 });
 
-// below path not working, returns undefined
-app.use(express.urlencoded({ extended: true }));
-
-app.post("/foo", function (req, res) {
-  let body = req.body;
-  console.log(body.foo);
-  res.send(body.foo);
-});
-
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
--- run_docker_locally.sh
+++ run_docker_locally.sh
@@ -1,2 +1,3 @@
 docker build -t chrt-express-postgres-tradingdata .
-docker run -it -p 9000:8080 chrt-express-postgres-tradingdata
+
+docker run -it -p 3000:8080 chrt-express-postgres-tradingdata
