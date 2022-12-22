527ec79d1f6c31de10f8fb640942bf9591f7d35e
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2022-12-22 14:52:42 -0600

    added /health path for health check

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+//-- Target Group Health Check --//
+app.get("/health", function (req, res) {
+  res.send("healthy!");
+});
+
 app.get("/data", function (req, res) {
   res.json({ foo: "bar" });
 });
