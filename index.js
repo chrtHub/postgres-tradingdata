f8b79a62989eece0bd264c278bca958e4f06b71c
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2022-12-26 15:07:11 -0600

    working on pg client connection

--- index.js
+++ index.js
@@ -54,6 +54,19 @@ async function getDatabasePasswordFromSecretsManager() {
     db_dbname = getSecretResponse.SecretString.dbname;
     db_username = getSecretResponse.SecretString.username;
     db_password = getSecretResponse.SecretString.password;
+    console.log(
+      "db_host: " +
+        db_host +
+        "\n" +
+        "db_port: " +
+        db_port +
+        "\n" +
+        "db_name: " +
+        db_name +
+        "\n" +
+        "db_username: " +
+        db_username
+    ); // DEV
   } catch (error) {
     console.log(error);
   }
@@ -61,26 +74,26 @@ async function getDatabasePasswordFromSecretsManager() {
 await getDatabasePasswordFromSecretsManager();
 
 //-- pg connect to RDS Instance --//
-const pgClient = new Client();
-await pgClient.connect();
+// const pgClient = new Client();
+// await pgClient.connect();
 
-const res = await pgClient.query("SELECT $1::text as message", [
-  "Hello world!",
-]);
-console.log(res.rows[0].message); // Hello world!
-await pgClient.end();
+// const res = await pgClient.query("SELECT $1::text as message", [
+//   "Hello world!",
+// ]);
+// console.log(res.rows[0].message); // Hello world!
+// await pgClient.end();
 
 //-- Configure pg Client to connect to RDS Instance --//
-// const client = new Client({
-//   host: `${db_host}`,
-//   port: `${db_port}`,
-//   database: `${db_dbname}`,
-//   user: `${db_username}`,
-//   password: `${db_password}`,
-// });
+const pgClient = new Client({
+  host: `${db_host}`,
+  port: `${db_port}`,
+  database: `${db_dbname}`,
+  user: `${db_username}`,
+  password: `${db_password}`,
+});
 
-// // run sample query
-// await pgClient.connect();
-// const res2 = await pgClient.query("SELECT * FROM employees LIMIT 5;");
-// console.log(res2.rows[0].message); // Alice, Bob, Charlie, Dave, Eve. Id, Name, Salary
-// await pgClient.end();
+// run sample query
+await pgClient.connect();
+const res2 = await pgClient.query("SELECT * FROM employees LIMIT 5;");
+console.log(res2.rows[0].message); // Alice, Bob, Charlie, Dave, Eve. Id, Name, Salary
+await pgClient.end();
