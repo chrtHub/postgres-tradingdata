d1d0a662837bfd9d98b9c57bd1c005e7d7085958
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2022-12-27 00:13:09 -0600

    working on pg client connection

--- foo.js
+++ foo.js
@@ -1,34 +1,8 @@
-//-- Express server --//
-import express from "express";
-
 //-- Secrets Manager --//
 import {
   SecretsManagerClient,
   GetSecretValueCommand,
 } from "@aws-sdk/client-secrets-manager";
-
-//-- Allow for a CommonJS "require" statement inside this ES Modules file --//
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-
-//-- Use "require" statement for 'pg' package used for PosgtgreSQL queries --//
-const { Client } = require("pg");
-
-//-- Express server --//
-const app = express();
-const PORT = 8080;
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/data", (req, res) => {
-  res.json({ foo: "bar" });
-});
-
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
 
 //-- Database password from Secrets Manager --//
 const secretsManager_client = new SecretsManagerClient({
@@ -61,19 +35,3 @@ async function getDatabasePasswordFromSecretsManager() {
   }
 }
 await getDatabasePasswordFromSecretsManager();
-
-//-- Configure pg Client to connect to RDS Instance --//
-const pgClient = new Client({
-  host: `${db_host}`,
-  port: `${db_port}`,
-  database: `${db_dbname}`,
-  user: `${db_username}`,
-  password: `${db_password}`,
-});
-await pgClient.connect(); // currently not connecting
-
-// run sample query
-const res2 = await pgClient.query("SELECT * FROM employees LIMIT 5;");
-console.log(res2.rows[0].message); // Alice, Bob, Charlie, Dave, Eve. Id, Name, Salary
-
-await pgClient.end();
