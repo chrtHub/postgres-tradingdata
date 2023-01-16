6c1a63cfb706a52d6cc35970528cccefa48999db
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-01-15 19:52:49 -0600

    removing express-sessions and connect-express, keeping old code in example file

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ app.use(cors(corsConfig));
 //-- Just-for-fun middleware --//
 app.use((req, res, next) => {
   res.append("Answer-to-Life-Universe-Everything", 42);
+  res.append("Foo", "Bar"); // DEV
   res.append("X-Powred-By", "Lisp (Arc)");
   next();
 });
