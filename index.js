7aadf5ac13a00e933c2aad115fe388f5c6c5c166
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-02-07 16:37:24 -0600

    added port 4173 on localhost to cors config

--- index.js
+++ index.js
@@ -80,6 +80,8 @@ const corsConfig = {
     "https://*.chrt.com",
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "http://127.0.0.1:4173",
+    "http://localhost:4173",
   ],
   maxAge: 3600,
 };
