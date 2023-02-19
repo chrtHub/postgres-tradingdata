5e05ef4f43bf01833b399f84e5c392faaaa475e6
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-02-19 17:25:42 -0600

    establishing ssh tunnel externally from server allows tunneling when in dev mode by overriding db host and port to be localhost and local ssh tunnel port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //-- *************** Imports *************** --//
 //-- Database config --//
 import { getDatabaseConfigFromSecretsManager } from "./App/config/dbConfig.js";
-import { Client as SSH_Client } from "ssh2"; //-- Dev mode, ssh tunnel to RDS instance --//
+// import { Client as SSH_Client } from "ssh2"; //-- Dev mode, ssh tunnel to RDS instance --//
 import fs from "fs";
 
 //-- Express server --//
@@ -31,61 +31,15 @@ console.log("process.env.NODE_ENV: " + process.env.NODE_ENV);
 let { db_host, db_port, db_username, db_password, db_dbname } =
   await getDatabaseConfigFromSecretsManager();
 
-//-- SSH tunnel config values --//
-let src_host = "localhost";
-let src_port = 2222;
-
-let ec2_username = "ec2-user";
-let ec2_host = "18.207.101.199"; //-- Subject to change --//
-let ec2_pemFile = fs.readFileSync("./chrt-1-bastion-postgres-02.pem"); //-- Local file --//
-
 //-- In development mode, connect to db via SSH tunnel --//
+//-- NOTE - must establish SSH tunnel before starting this server --//
 if (process.env.NODE_ENV === "development") {
-  console.log("To use SSH tunnell...");
-
-  const setupSSHTunnel = async () => {
-    return new Promise((resolve, reject) => {
-      //-- Establish ssh tunnel via EC2 bastion host to RDS --//
-      const ssh_conn = new SSH_Client();
-      ssh_conn.connect({
-        host: ec2_host,
-        username: ec2_username,
-        privateKey: ec2_pemFile,
-      });
-
-      //-- On ready, create forwarding --//
-      ssh_conn.on("ready", () => {
-        console.log("ssh connection :: ready");
-        ssh_conn.forwardOut(
-          src_host,
-          src_port,
-          db_host,
-          db_port,
-          (err, stream) => {
-            if (err) {
-              console.log("ssh_conn.forwardOut() err: " + err);
-              reject(err);
-            } else {
-              //-- Override db_host to point to local ssh tunnel, not directly to RDS --//
-              db_host = src_host;
-              db_port = src_port;
-
-              console.log("SSH tunnel (forwardOut) ready");
-              resolve();
-            }
-          }
-        );
-      });
-    });
-  };
-
-  await setupSSHTunnel();
+  db_host = "127.0.0.1";
+  db_port = 2222;
 }
 
-console.log(`db_host:db_port - ${db_host}:${db_port}`);
-
 //-- Knex --//
-console.log("knex requesting connection to postgres...");
+console.log(`knex requesting connection to postgres at ${db_host}:${db_port}`);
 const knex = require("knex")({
   client: "pg",
   connection: {
@@ -95,13 +49,6 @@ const knex = require("knex")({
     password: db_password,
     database: db_dbname,
   },
-  // ssh: {
-  //   host: ec2_host,
-  //   user: ec2_username,
-  //   privateKey: ec2_pemFile,
-  //   port: 22,
-  //   dstPort: db_port,
-  // },
 });
 //-- Export knex for use in controllers --//
 export { knex };
