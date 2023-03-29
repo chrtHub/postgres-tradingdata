fabfd6d46ecb267e3d6403b1ee9b39925c487cb1
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-03-29 00:38:06 -0500

    updated llmControllers gpt 3.5 route to receive a model name and an array of chatRequestMessages

--- App/controllers/llmControllers.ts
+++ App/controllers/llmControllers.ts
@@ -36,24 +36,17 @@ export const gpt35turboController = async (
   res: Response
 ) => {
   /** get prompt from request */
-  let prompt = req.body.prompt;
-  console.log(prompt); // DEV
+  let model = req.body.model;
+  let chatRequestMessages = req.body.chatRequestMessages;
 
   /** send prompt to OpenAI API */
   try {
     const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content:
-            "Your name is ChrtGPT. Refer to yourself as ChrtGPT. You are ChrtGPT, a helpful assistant that helps power a day trading performance journal. You sometimes make jokes and say silly things on purpose.",
-        },
-        { role: "user", content: prompt },
-      ],
+      model: model,
+      messages: chatRequestMessages,
     });
 
-    console.log("llm response received"); // DEV
+    console.log("llm response received", Date.now()); // DEV
 
     return res.status(200).json(response.data); // DEV
   } catch (err) {
