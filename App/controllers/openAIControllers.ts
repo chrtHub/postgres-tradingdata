78343147b4959b485299ebfbb911f95a41f106ff
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-04-07 13:37:32 -0500

    response streaming seems to work

--- App/controllers/openAIControllers.ts
+++ App/controllers/openAIControllers.ts
@@ -14,29 +14,6 @@ import { CreateChatCompletionResponse } from "openai";
 //-- OpenAI Client --//
 import { openai } from "../../index.js";
 
-// export const gpt35turboController = async (
-//   req: IRequestWithAuth,
-//   res: Response
-// ) => {
-//   /** get prompt from request */
-//   let model = req.body.model;
-//   let chatRequestMessages = req.body.chatRequestMessages;
-
-//   /** send prompt to OpenAI API */
-//   try {
-//     const response = await openai.createChatCompletion({
-//       model: model,
-//       messages: chatRequestMessages,
-//     });
-//     console.log("llm response received", Date.now()); // DEV
-//     console.log(response.data.model); // DEV
-//     return res.status(200).json(response.data); // DEV
-//   } catch (err) {
-//     console.log(err);
-//     return res.status(500).send("error during gpt35turboController llm query");
-//   }
-// };
-
 //-- Stream --//
 export const gpt35turboStreamController = async (
   req: IRequestWithAuth,
@@ -44,22 +21,18 @@ export const gpt35turboStreamController = async (
 ) => {
   console.log("gpt35turboStreamController() called"); // DEV
 
-  /** get prompt from request */
+  //-- Get model and messages from request --//
   let model = req.body.model;
   let chatRequestMessages = req.body.chatRequestMessages;
 
-  //-- parser --//
-  function onParse(event: any) {
-    if (event.type === "event") {
-      if (event.data !== "[DONE]") {
-        console.log(JSON.parse(event.data).choices[0].delta?.content || ""); // DEV
-        // res.write(JSON.parse(event.data).choices[0].delta?.content || ""); // DEV
-      }
-    } else if (event.type === "reconnect-interval") {
-      console.log("%d milliseconds reconnect interval", event.value);
-    }
-  }
-  const parser = createParser(onParse);
+  //-- Set headers needed for SSE --//
+  res.set({
+    "Cache-Control": "no-cache",
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+  });
+  res.flushHeaders(); //-- Send headers immediately (don't wait for first chunk or message end) --//
+
   try {
     //-- Get API Key --//
     let OPENAI_API_KEY = await getOpenAI_API_Key();
@@ -88,13 +61,58 @@ export const gpt35turboStreamController = async (
     response.data.on("data", (chunk: any) => readableStream.push(chunk));
     response.data.on("end", () => readableStream.push(null));
 
-    //-- Decode chunks from readableStream and feed them to the parser --//
+    //-- Create text decoder (binary --> string) --//
+    //-- UInt8Array | ArrayBuffer | Buffer --> UTF-8 (default) --//
     const textDecoder = new TextDecoder();
+
+    //-- Decode readableStream chunks and feed them to the parser --//
     readableStream.on("data", (chunk) => {
       parser.feed(textDecoder.decode(chunk));
     });
+
+    //-- Create parser --//
+    const parser = createParser(onParse);
+
+    function onParse(event: any) {
+      if (event.type === "event") {
+        if (event.data !== "[DONE]") {
+          let data = JSON.parse(event.data).choices[0].delta?.content || "";
+          res.write(`data: ${data}\n\n`); //-- Send data to client --//
+          // console.log(data); // DEV
+        } else if (event.data === "[DONE]") {
+          res.end(); //-- Close connection --//
+        }
+      } else if (event.type === "reconnect-interval") {
+        console.log("%d milliseconds reconnect interval", event.value);
+      }
+    }
+    //----//
   } catch (err) {
     console.log(err);
     res.status(500).send("error during gpt35turboStreamController llm query");
   }
 };
+
+//-- Non-streaming (sends whole response) --//
+// export const gpt35turboController = async (
+//   req: IRequestWithAuth,
+//   res: Response
+// ) => {
+//   /** get prompt from request */
+//   let model = req.body.model;
+//   let chatRequestMessages = req.body.chatRequestMessages;
+
+//   /** send prompt to OpenAI API */
+//   try {
+//     const response = await openai.createChatCompletion({
+//       model: model,
+//       messages: chatRequestMessages,
+//     });
+//     console.log("llm response received", Date.now()); // DEV
+//     console.log(response.data.model); // DEV
+//     return res.status(200).json(response.data); // DEV
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(500).send("error during gpt35turboController llm query");
+//   }
+// };
