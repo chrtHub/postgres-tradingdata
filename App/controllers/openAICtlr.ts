f7c0695415ab0ab6dbb7cbd20d4ca6e512e2133e
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-04-25 19:38:01 -0500

    implementing tree-based chat

--- App/controllers/openAICtlr.ts
+++ App/controllers/openAICtlr.ts
@@ -169,47 +169,176 @@ export const gpt35TurboSSEController = async (
     },
   ];
 
-  // build conversation tree from message_nodes
+  //-- Start tracking request_message_node_ids and request_tokens --//
+  const request_messages_node_ids: ObjectId[] = [];
+  request_messages_node_ids.push(new_message_node._id);
+  let request_tokens: number = 0;
+  request_tokens += tiktoken(root_node.prompt.content);
+  request_tokens += tiktoken(new_message_node.prompt.content);
 
+  // build conversation tree from message_nodes
   // // starting from the parent_node_id, add up to 3000 total tokens by recursively getting the prompt+completion pair from each parent node (don't add single messages, only pairs)
+
+  // request_tokens += tiktoken(message)
   // request_messages.splice(1, 0, message)
 
   // // // for each node added, add the node_id to request_messages_node_ids
-  // // // calculate the total tokens as the propmt_tokens
+  // // // calculate the total tokens as the prompt_tokens
+
+  let api_req_res_metadata: IAPIReqResMetadata;
+
+  let completion_content: string;
+
+  //-- Set headers --//
+  res.set({
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+    "Content-Type": "text/event-stream",
+    "Access-Control-Expose-Headers":
+      "CHRT-conversation-id, CHRT-new-message-node-id, CHRT-new-message-created-at, CHRT-parent-node-id",
+    "CHRT-conversation-id": new_message_node.conversation_id,
+    "CHRT-new-message-node-id": new_message_node._id,
+    "CHRT-new-message-created-at": new_message_node.created_at,
+    "CHRT-parent-node-id": new_message_node.parent_node_id,
+  });
+  //-- Send headers immediately (don't wait for first chunk or message end) --//
+  res.flushHeaders();
 
   // LLM request - send request_messages to LLM
-
-  // create a completion_message with message = ""
-
-  // upon completion start
-  // // new conversation - send conversation_id and message_node w/o completion message
-  // // continuting conversation - n/a
-
-  // proxy stream SSE completion message
-  // // build up completion_message message
-
-  // when SSE done
-
-  let completion: IMessage = {
-    author: model.api_name,
-    model: model,
-    created_at: new Date(),
-    role: "assistant",
-    content: completion_content,
-  };
-
-  //-- Update new_message_node by adding completion --//
-  root_node = produce(new_message_node, (draft) => {
-    draft.completion = completion;
-  });
-
-  //-- Write new_message_node to database --//
   try {
-    await Mongo.message_nodes.insertOne(new_message_node);
+    //-- Get API Key --//
+    let OPENAI_API_KEY = await getOpenAI_API_Key();
+
+    // TODO - add type here, e.g. Interface for OpenAI Chat Completions Request Body. Allow parameters such as temperature, etc.
+    const request_body = {
+      model: model.api_name,
+      messages: request_messages,
+      stream: true,
+    };
+
+    //-- Axios POST request to OpenAI --//
+    let response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      request_body,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${OPENAI_API_KEY}`,
+        },
+        responseType: "stream",
+      }
+    );
+
+    //-- Create readable stream --//
+    const readableStream = new Readable({
+      read() {},
+    });
+
+    //-- Feed response.data to readableStream --//
+    response.data.on("data", (chunk: any) => readableStream.push(chunk));
+    response.data.on("end", () => readableStream.push(null));
+
+    //-- Create text decoder (binary --> string) --//
+    //-- UInt8Array | ArrayBuffer | Buffer --> UTF-8 (default) --//
+    const textDecoder = new TextDecoder();
+
+    //-- Create parser --//
+    const parser = createParser(onParse);
+
+    //-- Decode readableStream chunks and feed them to the parser --//
+    readableStream.on("data", (chunk) => {
+      parser.feed(textDecoder.decode(chunk));
+    });
+
+    //-- Use array to save response to MongoDB when streaming finishes --//
+    const completion_chunks: string[] = [];
+
+    //-- Inside parser, send each chunk to the client, then close the connection --//
+    async function onParse(event: any) {
+      if (event.type === "event") {
+        if (event.data !== "[DONE]") {
+          let data = JSON.parse(event.data).choices[0].delta?.content || "";
+
+          //-- Add chunk to response chunks (to be accessed post-stream) --//
+          completion_chunks.push(data);
+
+          //-- URI Encode to avoid content and delimeter `\n\n` collisions --//
+          const uriEncodedData = encodeURI(data);
+
+          //-- Send data to the client --//
+          res.write(`data: ${uriEncodedData}\n\n`);
+          //----//
+        } else if (event.data === "[DONE]") {
+          //-- Build completion --//
+          const completion_content = completion_chunks.join("");
+          const completion_tokens = tiktoken(completion_content.toString());
+          const completion_created_at = new Date();
+          const completion: IMessage = {
+            author: model.api_name,
+            model: model,
+            created_at: completion_created_at,
+            role: "assistant",
+            content: completion_content,
+          };
+
+          //-- Update new_message_node by adding completion --//
+          root_node = produce(new_message_node, (draft) => {
+            draft.completion = completion;
+          });
+
+          //-- Write new_message_node to database --//
+          try {
+            await Mongo.message_nodes.insertOne(new_message_node);
+          } catch (err) {
+            console.log(err);
+            throw new Error("error storing new message_node");
+          }
+          //-- Write api_req_res_metadata as update to conversation --//
+          try {
+            await Mongo.conversations.updateOne(
+              { _id: new_message_node.conversation_id },
+              { $addToSet: { api_req_res_metadata: api_req_res_metadata } }
+            );
+          } catch (err) {
+            console.log(err);
+            throw new Error(
+              "error adding api_req_res_metadata to conversation"
+            );
+          }
+
+          //-- Send completion to client --//
+          const completion_string = JSON.stringify(completion);
+          res.write(`id: completion_message\ndata: ${completion_string}\n\n`);
+
+          //-- Update api_req_res_metadata --//
+          api_req_res_metadata = {
+            user: user_db_id,
+            model_api_name: model.api_name,
+            created_at: new Date(),
+            request_tokens: request_tokens,
+            completion_tokens: completion_tokens,
+            total_tokens: request_tokens + completion_tokens,
+            node_id: new_message_node._id,
+            request_messages_node_ids: request_messages_node_ids,
+          };
+
+          //-- Close connection --//
+          res.end();
+        }
+      } else if (event.type === "reconnect-interval") {
+        console.log("%d milliseconds reconnect interval", event.value);
+      }
+    }
+    //----//
   } catch (err) {
     console.log(err);
-    throw new Error("error storing new message_node");
-  }
-  // // update conversation with APIReqResMetadata - append via update or append using immer then update?
-  // // send completion_message to the client
+    res.write(
+      `id: error\ndata: ${JSON.stringify({
+        message: "error during gpt35turboStreamController LLM query",
+        error: err,
+      })}\n\n`
+    );
+    //-- Close the connection after sending the error message --//
+    res.end();
+  } // TODO - test that frontend parses this correctly
 };
