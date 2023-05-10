95f5da0309fc8962abf2b902b4daacdf81928634
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-05-10 01:40:15 -0500

    create_title working

--- App/controllers/openAIControllers.ts
+++ App/controllers/openAIControllers.ts
@@ -108,85 +108,87 @@ export const createTitleController = async (
   res: Response
 ) => {
   console.log("----- createTitle -----");
-
   //-- Get data from params --//
   let { conversation_id } = req.body;
   let user_db_id = getUserDbId(req);
-  let prompt: string = "";
-  let completion: string = "";
   let new_title: string = "TODO - new title";
 
   try {
-    let response: IMessageNode_Mongo[] = await Mongo.message_nodes
+    let message_node: IMessageNode_Mongo[] = await Mongo.message_nodes
       .find({
-        conversation_id: conversation_id,
+        conversation_id: ObjectId.createFromHexString(conversation_id),
         user_db_id: user_db_id, //-- Security --//
       })
       .sort({ created_at: -1 })
       .limit(1)
       .toArray();
-    let message_node = demongoize_message_node(response[0]);
-    prompt = message_node.prompt.content;
-    completion = message_node.completion?.content || "";
-  } catch (err) {
-    console.log(err);
-  }
-  //-- Get API Key --//
-  let OPENAI_API_KEY = await getOpenAI_API_Key();
 
-  const request_body: CreateChatCompletionRequest = {
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "Please use the following messages to create a chat conversation title. Limit the title to approximately 60 characters. Be professional at all times and do not ever use profanity, explicit language, or sexual language when creating a title. Match the language of the messages, but if in doubt, use English.",
-      },
-      {
-        role: "user",
-        content: prompt,
-      },
-      { role: "assistant", content: completion },
-    ],
-    user: await getSHA256Hash(user_db_id),
-  };
-
-  // use LLM request to generate a title
-  try {
-    //-- Axios POST request to OpenAI --//
-    let response = await axios.post(
-      "https://api.openai.com/v1/chat/completions",
-      request_body,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${OPENAI_API_KEY}`,
+    //-- Build request body --//
+    const request_body: CreateChatCompletionRequest = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "Please create a chat conversation title by capturing the essence of the following message(s). Use approximately 50 characters or less. Do not include anything else in your response - only include the exact text of the chat conversation title.",
         },
-        responseType: "stream",
+        {
+          role: "user",
+          content: `User's prompt: ${
+            message_node[0].prompt.content
+          }. Assistant's first completion: ${
+            message_node[0].completion?.content || ""
+          }`,
+        },
+      ],
+      user: await getSHA256Hash(user_db_id),
+    };
+
+    //-- Make LLM request to generate a title --//
+    let OPENAI_API_KEY = await getOpenAI_API_Key();
+    try {
+      //-- Axios POST request to OpenAI --//
+      let chat_completion = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        request_body,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${OPENAI_API_KEY}`,
+          },
+        }
+      );
+      new_title = chat_completion.data.choices[0].message.content;
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Error calling LLM to create title");
+    }
+
+    //-- Enforce title max length 60 chars. If char 60 is whitespace, remove it. --//
+    if (new_title.length > 60) {
+      new_title = new_title.substring(0, 60).trim();
+      if (new_title.endsWith(" ")) {
+        new_title = new_title.substring(0, new_title.length - 1);
       }
-    );
-    new_title = "TODO ";
+      new_title += "...";
+    }
+    console.log("todo - set title to: ", new_title); // DEV
+    try {
+      await Mongo.conversations.updateOne(
+        {
+          _id: ObjectId.createFromHexString(conversation_id),
+          user_db_id: user_db_id, //-- security --//
+        },
+        { $set: { title: new_title } }
+      );
+      return res.send(`title updated to: ${new_title}`);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send("Error writing title to database");
+    }
   } catch (err) {
     console.log(err);
-  }
-
-  //-- Enforce title max length 60 chars --//
-  if (new_title.length > 60) {
-    new_title = new_title.substring(0, 60) + "...";
-  }
-
-  try {
-    let response = await Mongo.conversations.updateOne(
-      {
-        _id: ObjectId.createFromHexString(conversation_id),
-        user_db_id: user_db_id, //-- security --//
-      },
-      { $set: { title: new_title } }
-    );
-    return res.send(`title updated to: ${new_title}`);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send("error creating title");
+    res.status(500).send("Error fetching most recent message node");
   }
 };
 
