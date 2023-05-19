922277d8214cf9dcdf7dacd8ec36c98697a07960
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-05-18 21:59:08 -0500

    working on error handling sandbox

--- App/controllers/errorControllers.ts
+++ App/controllers/errorControllers.ts
@@ -4,21 +4,68 @@ import getUserDbId from "../utils/getUserDbId.js";
 import { Response } from "express";
 import { IRequestWithAuth } from "../../index.d";
 
-//-- Error --//
+//-- 400 Error --//
+export const backend400Error = async (req: IRequestWithAuth, res: Response) => {
+  try {
+    throw new Error();
+  } catch (e) {
+    res.status(400).send("hello, this is a test backend 400 error");
+    // res
+    //   .status(400)
+    //   .json({ message: "hello, this is a test backend 400 error" });
+  }
+};
+
+//-- 401 Error --//
+export const backend401Error = async (req: IRequestWithAuth, res: Response) => {
+  try {
+    throw new Error();
+  } catch (e) {
+    res.status(401).send("hello, this is a test backend 401 error");
+  }
+};
+
+//-- 403 Error --//
+export const backend403Error = async (req: IRequestWithAuth, res: Response) => {
+  try {
+    throw new Error();
+  } catch (e) {
+    res.status(403).send("hello, this is a test backend 403 error");
+  }
+};
+
+//-- 404 Error --//
+export const backend404Error = async (req: IRequestWithAuth, res: Response) => {
+  try {
+    throw new Error();
+  } catch (e) {
+    res.status(404).send("hello, this is a test backend 404 error");
+  }
+};
+
+//-- 408 Error --//
+export const backend408Error = async (req: IRequestWithAuth, res: Response) => {
+  try {
+    throw new Error();
+  } catch (e) {
+    res.status(408).send("hello, this is a test backend 408 error");
+  }
+};
+
+//-- 418 Error --//
 export const backend418Error = async (req: IRequestWithAuth, res: Response) => {
-  res.status(418).send("418 error");
+  try {
+    throw new Error();
+  } catch (e) {
+    res.status(418).send("hello, this is a test backend 418 error");
+  }
 };
 
 //-- 500 Error --//
 export const backend500Error = async (req: IRequestWithAuth, res: Response) => {
-  let user_db_id = getUserDbId(req);
-
   try {
     throw new Error();
   } catch (e) {
-    // res.status(500).send("hello, this is a test backend 500 error");
-    res
-      .status(500)
-      .json({ message: "hello, this is a test backend 500 error" });
+    res.status(500).send("hello, this is a test backend 500 error");
   }
 };
--- App/routes/errorRoutes.ts
+++ App/routes/errorRoutes.ts
@@ -6,7 +6,13 @@ import * as ctrl from "../controllers/errorControllers.js";
 const router = express.Router();
 
 //-- Routes --//
-router.get("/", ctrl.backend418Error);
+router.get("/", ctrl.backend400Error);
+router.get("/400", ctrl.backend400Error);
+router.get("/401", ctrl.backend401Error);
+router.get("/403", ctrl.backend403Error);
+router.get("/404", ctrl.backend404Error);
+router.get("/408", ctrl.backend408Error);
+router.get("/418", ctrl.backend418Error);
 router.get("/500", ctrl.backend500Error);
 
 //-- Export --//
