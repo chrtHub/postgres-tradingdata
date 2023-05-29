1a1519863eb6fea7a712e5d04d9fd6a2fd0fad7d
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-05-29 01:29:00 -0500

    cleanup

--- App/Auth/journalAuthMiddleware.ts
+++ App/Auth/journalAuthMiddleware.ts
@@ -7,18 +7,15 @@ export const journalAuthMiddleware = async (
   next: NextFunction
 ) => {
   let payload = req?.auth?.payload;
-  let read_journal: boolean = false;
   let write_journal: boolean = false;
 
   //-- Check for necessary permissions to access route(s) guarded by this middleware --//
   if (payload && payload.permissions) {
-    read_journal = payload.permissions.includes("read:journal");
     write_journal = payload.permissions.includes("write:journal");
   }
 
   //-- Only proceed via 'next()' if necessary permissions are present, otherwise send 401 --//
-  if (read_journal && write_journal) {
-    res.append("CHRT-JWT-permission-read_journal", "verified");
+  if (write_journal) {
     res.append("CHRT-JWT-permission-write_journal", "verified");
     next();
   } else {
