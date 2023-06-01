98cf439c4cb447977082aeb7dc4623d4970d3166
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-06-01 17:48:36 -0500

    updated countTokens

--- App/controllers/chatson/countTokens.ts
+++ App/controllers/chatson/countTokens.ts
@@ -2,8 +2,8 @@
  * A faster alternative to tiktoken. Estimates tokens in a string by counting 4.7 chars (client-side) or 4.5 chars (server-side) as a token. The server counts faster (more strictly).
  */
 export function countTokens(message: string): number {
-  const serverSideTokenLength = 4.5;
   // const clientSideTokenLength = 4.7;
+  const serverSideTokenLength = 4.6;
   const messageLength = message.length;
   const tokens = Math.ceil(messageLength / serverSideTokenLength);
 
