ee5e51019d23059b8bb7877e01921a9fc7f63f9a
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-06-02 21:33:47 -0500

    changing /llm route to /conversation, putting middlewares in routes files, added stableDiffusionAuthMiddleware

--- App/controllers/OpenAI/createImage.ts
+++ App/controllers/OpenAI/createImage.ts
@@ -22,61 +22,19 @@ export const createImage = async (req: IRequestWithAuth, res: Response) => {
   //     return res.status(400).send("Missing foo param");
   //   }
 
-  //-- Ad lib inputs --//
-  let animal = [
-    "dog",
-    "cat",
-    "puppy",
-    "kitten",
-    "bear",
-    "cow",
-    "squirrel",
-    "koala bear",
-    "penguin",
-    "sloth",
-  ];
-  let emotion = [
-    "worried",
-    "nervous",
-    "perplexed",
-    "confused",
-    "disappointed",
-    "jealous",
-    "frustrated",
-    "anxious",
-    "sad",
-    "furious",
-  ];
-  let style = [
-    "realism",
-    "minimalism",
-    "retro/vintage",
-    "lettering",
-    "geometric",
-    "flat art",
-    "3D illustration",
-    "surrealism",
-    "psychedelic",
-  ];
-
-  let selectedAnimal = animal[Math.floor(Math.random() * animal.length)];
-  let selectedEmotion = emotion[Math.floor(Math.random() * emotion.length)];
-  let selectedStyle = style[Math.floor(Math.random() * style.length)];
+  const foo = "foo";
 
   try {
     await retry(
       async () => {
         const response = await OpenAIClient.createImage({
-          prompt: `a cute ${selectedAnimal} that is feeling ${selectedEmotion} because its computer crashed, ${selectedStyle}`,
+          prompt: `this is the prompt as a string template literal ${foo}`,
           n: 1,
           size: "512x512",
           response_format: "b64_json",
         });
         let base64JSON = response.data.data[0].b64_json;
         res.status(200).json({
-          animal: selectedAnimal,
-          emotion: selectedEmotion,
-          style: selectedStyle,
           base64JSON: base64JSON,
         });
       },
--- App/routes/openAIRoutes.ts
+++ App/routes/openAIRoutes.ts
@@ -1,33 +1,49 @@
 //-- ********** Imports ********** --//
 //-- ***** ***** ***** ***** ***** --//
 import express from "express";
-// import * as ctrl from "../controllers/openAICreateTitle.js";
-import { createTitle } from "../controllers/OpenAI/createTitle.js";
-import { createImage } from "../controllers/OpenAI/createImage.js";
+
+//-- Proxy-ish Routes --//
 import { chatCompletionsSSE } from "../controllers/OpenAI/chatCompletionSSE.js";
+// import { createImage } from "../controllers/OpenAIStableDiffusion/createImage.js";
+
+//-- Routes with Custom Functionality --//
+import { createTitle } from "../controllers/OpenAI/createTitle.js";
 
 //-- Express router --//
 const router = express.Router();
 
+//-- Middlewares --//
+import { llmAuthMiddleware } from "../Auth/llmAuthMiddleware.js";
+// import { stableDiffusionAuthMiddleware } from "../Auth/stableDiffusionAuthMiddleware.js";
+
+//-- Types ---//
 import { Response } from "express";
 import { IRequestWithAuth } from "../../index.d";
 
-//-- ********** Routes ********** --//
-//-- ***** ***** ***** ***** ***** --//
-
-router.post("/v1/chat/completions", (req: IRequestWithAuth, res: Response) => {
-  //-- If request has `accept: text/event-stream` header, use the SSE controller --//
-  const acceptHeader = req.get("Accept") || req.get("accept");
-  if (acceptHeader && acceptHeader.includes("text/event-stream")) {
-    return chatCompletionsSSE(req, res);
-  } else {
-    // return ctrl.gpt35TurboController(req, res);
+//-- ********** Proxy-ish Routes ********** --//
+router.post(
+  "/v1/chat/completions",
+  llmAuthMiddleware,
+  (req: IRequestWithAuth, res: Response) => {
+    //-- If request has `accept: text/event-stream` header, use the SSE controller --//
+    const acceptHeader = req.get("Accept") || req.get("accept");
+    if (acceptHeader && acceptHeader.includes("text/event-stream")) {
+      return chatCompletionsSSE(req, res);
+    } else {
+      // return ctrl.gpt35TurboController(req, res);
+    }
   }
-});
+);
 
-router.post("/create_title", createTitle);
+// TODO - implement controller
+// router.post(
+//   "/v1/images/generations",
+//   stableDiffusionAuthMiddleware,
+//   createImage
+// );
 
-router.post("/create_image", createImage);
+//-- ********** Routes with Custom Functionality ********** --//
+router.post("/create_title", llmAuthMiddleware, createTitle);
 
 //-- ********** Export ********** --//
 export default router;
