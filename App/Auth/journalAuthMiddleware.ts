bbf23675d676ff04d6b0a97d361bdd1c9b26f416
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-06-05 21:26:11 -0500

    added read:journal to journal auth middleware

--- App/Auth/journalAuthMiddleware.ts
+++ App/Auth/journalAuthMiddleware.ts
@@ -7,15 +7,18 @@ export const journalAuthMiddleware = async (
   next: NextFunction
 ) => {
   let payload = req?.auth?.payload;
+  let read_journal: boolean = false;
   let write_journal: boolean = false;
 
   //-- Check for necessary permissions to access route(s) guarded by this middleware --//
   if (payload && payload.permissions) {
+    read_journal = payload.permissions.includes("read:journal");
     write_journal = payload.permissions.includes("write:journal");
   }
 
   //-- Only proceed via 'next()' if necessary permissions are present, otherwise send 401 --//
-  if (write_journal) {
+  if (read_journal && write_journal) {
+    res.append("CHRT-JWT-permission-read_journal", "verified");
     res.append("CHRT-JWT-permission-write_journal", "verified");
     next();
   } else {
