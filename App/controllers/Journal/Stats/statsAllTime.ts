dd6305ab0f59329b5e77b2bfa2031f0228e28c3b
Aaron Carver <59704103+aaronmaxcarver@users.noreply.github.com>
2023-06-16 23:05:28 -0500

    /journal/stats/all_time route returns count of unique trades

--- App/controllers/Journal/Stats/statsAllTime.ts
+++ App/controllers/Journal/Stats/statsAllTime.ts
@@ -24,11 +24,26 @@ export const statsAllTime = async (req: IRequestWithAuth, res: Response) => {
   try {
     await retry(
       async () => {
-        const result = await knex("tradingdata02").count(
-          "trade_uuid as total_trades"
-        );
-        const totalTrades = result[0].total_trades;
-        return res.status(200).json({ totalTrades: totalTrades });
+        const qry_total_trades = await knex("tradingdata02")
+          .countDistinct("trade_uuid as total_trades")
+          .where("user_db_id", user_db_id);
+        const total_trades = qry_total_trades[0].total_trades;
+
+        const qry_total_symbols = await knex("tradingdata02")
+          .countDistinct("symbol as symbols")
+          .where("user_db_id", user_db_id);
+        const total_symbols = qry_total_symbols[0].symbols;
+
+        const qry_total_types = await knex("tradingdata02")
+          .countDistinct("trade_type as total_types")
+          .where("user_db_id", user_db_id);
+        const total_types = qry_total_types[0].total_types;
+
+        return res.status(200).json({
+          total_trades: total_trades,
+          total_symbols: total_symbols,
+          total_types: total_types,
+        });
       },
       {
         retries: 1,
